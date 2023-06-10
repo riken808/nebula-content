f829d09e08eb9c692724b28e2134bf77f55c4ec0
riken808 <rikenach@gmail.com>
2023-06-10 15:55:27 -0400

    homework

---
+++ 3-strings/1-string.assignment.js
@@ -0,0 +1,55 @@
+
+// 1. Concatenate "hello" with "goodbye"
+ const string1 = "hello"
+ const string2  = "goodbye"
+
+// 2. Save the substring "Friend" from the string "Hello Friend"
+const greeting = "Hello Friend"
+
+// 3. Using 5 or more of the following words, create a template literal that makes a legible sentence.
+//    Do not create your own words, use the existing ones below!
+const word1 = "hello"; 
+const word2 = "horror";
+const word3 = "peace";
+const word4 = "world"
+const word5 = "dislike"; 
+const word6 = "umbrella"; 
+const word7 = "car"; 
+const word8 = "pasta"; 
+const word9 = "pizza"; 
+const word10 = "movies";
+const word11 = "football"; 
+const word12 = "soccer"; 
+const word13 = "eat"; 
+const word14 = "cookie"; 
+const word15 = "favorite";
+const word16 = "makeup"; 
+const word17 = "I"; 
+const word18 = "to"; 
+const word19 = "my";
+const word20 = "is";
+const word21 = "and";
+const word22 = "also";
+const word23 = "like"; 
+
+// 4. Given the string of your full name get your initials.
+
+
+// 5. Get the 5th character from the following string: 'Hello World'
+const greeting1 = 'Hello World'
+
+// 6. Find the index of the dash from 'Coding-Time!'
+const coding = 'Coding-Time!'
+
+// 7. Write code to grab the final letter from a any string.
+const string = "string"
+
+// 8. Write code to grab the first 3 letters from a string. 
+
+
+// 9. Turn the following string into an array of words: 'This-is-a-sentence';
+const sentence1 = 'This-is-a-sentence'
+
+// 10. Make the string of 12 from the following two numbers:
+let one = 1;
+let two = 2;
---
+++ new_folder_creation.sh
@@ -0,0 +1,97 @@
+#!/bin/bash
+
+mkdir 0-data-types 1-variables 2-numbers 3-strings 4-control-flow 5-arrays 6-loops 7-functions 8-objects 9-html 10-css 11-dom-manipulation 12-classes 13-higher-order-functions 14-big-o 15-promises 16-regex 17-recursion 18-canvas 19-react 20-web-api 21-postgres 22-servers
+
+
+
+cd 0-data-types
+mkdir notes assignments practice
+cd ..
+
+cd 1-variables
+mkdir notes assignments practice
+cd ..
+
+cd 2-numbers
+mkdir notes assignments practice
+cd ..
+
+cd 3-strings
+mkdir notes assignments practice
+cd ..
+
+cd 4-control-flow
+mkdir notes assignments practice
+cd ..
+
+cd 5-arrays
+mkdir notes assignments practice
+cd ..
+
+cd 6-loops
+mkdir notes assignments practice
+cd ..
+
+cd 7-functions
+mkdir notes assignments practice
+cd ..
+
+cd 8-objects
+mkdir notes assignments practice
+cd ..
+
+cd 9-html 
+mkdir notes assignments practice
+cd ..
+
+cd 10-css 
+mkdir notes assignments practice
+cd ..
+
+cd 11-dom-manipulation 
+mkdir notes assignments practice
+cd ..
+
+cd 12-classes 
+mkdir notes assignments practice
+cd ..
+
+cd 13-higher-order-functions 
+mkdir notes assignments practice
+cd ..
+
+cd 14-big-o 
+mkdir notes assignments practice
+cd ..
+
+cd 15-promises 
+mkdir notes assignments practice
+cd ..
+
+cd 16-regex 
+mkdir notes assignments practice
+cd ..
+
+cd 17-recursion 
+mkdir notes assignments practice
+cd ..
+
+cd 18-canvas 
+mkdir notes assignments practice
+cd ..
+
+cd 19-react 
+mkdir notes assignments practice
+cd ..
+
+cd 20-web-api 
+mkdir notes assignments practice
+cd ..
+
+cd 21-postgres 
+mkdir notes assignments practice
+cd ..
+
+cd 22-servers
+mkdir notes assignments practice
+cd ..
