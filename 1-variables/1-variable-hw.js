acf93eb46b88f065775d5b53e35bb73b2d4b349b
riken808 <rikenach@gmail.com>
2023-06-10 16:04:59 -0400

    message

---
+++ 1-variables/1-variable-hw.js
@@ -0,0 +1,25 @@
+// Create a .js file that satisfies assignment below 
+// Copy the final assignment text into this assignment to submit
+// For each exercise you'll want to log the result to the console to check your work
+
+
+//1. Create a set of variables that define an animal (age, height, name, etc...). Must be at least 6 variables.
+let age = "0-130"
+let height = "1ft-9feet"
+const name = "Human"
+const classification = "Mammalia"
+let strenth = "Brain"
+let weakness = "Micro Oragnisms"
+let funfact = "human indurance in terms traveling long distances on their feet is comparable to horses, wolves, dogs, and camels also with proper training humans can swim for over a 150 miles in a single swim " 
+
+
+
+//2. Create a set of variables that define a vehicle. Must be at least 6 variables.
+const carName = "Porsche"
+let pros = "Driving expericnce/Performace is excellent and the craftsmanship is a work of art"
+let cons = "lacks utility"
+const founder = "Ferdinnand Porshe"
+let topspeed = "230mph"
+let appeal = "Iconic desigin, Quality, Motorsport success, eniginnering excellence" 
+let fact = "porsche was tasked by hitler to create volkswagen durning the nazi occupation they were also asked to create a afforable car for the soviet union by stalin but they chose not to"
+ 
